926a801ca32b226101f5c9ae57a8ad3779b9531a
theowiik <theo.le.wiik@gmail.com>
2023-11-24 15:40:22 +0100

    Remove unnecessary SVG files and add project data to the page

---
+++ src/app/models/project.ts
@@ -0,0 +1,7 @@
+export interface Project {
+  id: number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  tags: string[];
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
+import { Project } from './models/project';
+
 export default function Home() {
+  const projects: Project[] = [
+    {
+      id: 1,
+      name: 'EcoSolutions',
+      shortDescription: 'Eco-friendly product solutions',
+      description:
+        'Developing sustainable and eco-friendly products for a greener future.',
+      tags: ['environment', 'sustainability', 'green'],
+    },
+    {
+      id: 2,
+      name: 'TechTrends',
+      shortDescription: 'Latest in technology',
+      description:
+        'A project dedicated to exploring and implementing the latest technological trends.',
+      tags: ['technology', 'innovation', 'future'],
+    },
+    {
+      id: 3,
+      name: 'HealthPlus',
+      shortDescription: 'Healthcare innovations',
+      description:
+        'Focused on creating innovative solutions to modern healthcare challenges.',
+      tags: ['health', 'innovation', 'well-being'],
+    },
+  ];
+
   return (
     <main>
       <p>hello world</p>
+
+      {projects.map((project) => (
+        <div key={project.id}>
+          <h1>{project.name}</h1>
+          <h1>{project.description}</h1>
+        </div>
+      ))}
     </main>
-  )
+  );
 }
